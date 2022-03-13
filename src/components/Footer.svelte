dd7f41e0c836432224a212a568ebd918d96da123
qxb3 <toolong972@gmail.com>
2022-03-14 07:52:15 +0800

    Added repo link on footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,14 +1,24 @@
 <footer>
-	<h3>How to join?</h3>
-	<p>Click the <strong>Join now</strong> button to join our official discord server and go to the <strong>#apply-to-slackers</strong> channel</p>
-	<button on:click={() => window.open('https://discord.gg/74uEjq4rZE', '_blank')}>Join now</button>
+	<div class='content-container'>
+		<h3>How to join?</h3>
+		<p>Click the <strong>Join now</strong> button to join our official discord server and go to the <strong>#apply-to-slackers</strong> channel</p>
+		<button on:click={() => window.open('https://discord.gg/74uEjq4rZE', '_blank')}>Join now</button>
+	</div>
+	<div class='links-container'>
+		<a href='https://github.com/qxb3/slackers' target='_blank'>
+			<img src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt='repository' />
+		</a>
+	</div>
 </footer>
 
 <style>
 	footer {
 		background-color: #2c2f33;
 		color: white;
-		padding: 32px;
+	}
+
+	.content-container {
+		padding: 32px 32px 12px 32px;
 		text-align: center;
 	}
 
@@ -20,5 +30,16 @@
 		color: white;
 		box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
 		transition: 0.6;
+	}
+
+	.links-container {
+		display: flex;
+		justify-content: end;
+	}
+
+	img {
+		margin: 8px;
+		width: 40px;
+		height: 40px;
 	}
 </style>
