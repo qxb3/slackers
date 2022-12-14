934fe8d03d8802a0ad1e232d6a3288374acf1c9b
qxb3 <qxbthree@gmail.com>
2022-12-14 14:41:59 +0800

    hide members

--- src/routes/Members.svelte
+++ src/routes/Members.svelte
@@ -11,61 +11,10 @@
 
 <div class="container">
 	<div class="members">
-		<div class="group">
-			<h1 class="title is-size-4 mb-3">Leaders</h1>
-			<ol>
-				{#each leaders as leader}
-					<li>{leader}</li>
-				{/each}
-			</ol>
-		</div>
-
-		<div class="group">
-			<h1 class="title is-size-4 mb-3">Commanders</h1>
-			<ol>
-				{#each commanders as commander}
-					<li>{commander}</li>
-				{/each}
-			</ol>
-		</div>
-
-		<div class="group">
-			<h1 class="title is-size-4 mb-3">Knights</h1>
-			<ol>
-				{#each knights as knight}
-					<li>{knight}</li>
-				{/each}
-			</ol>
-		</div>
-
-		<div class="group">
-			<h1 class="title is-size-4 mb-3">Rangers</h1>
-			<ol>
-				{#each rangers as ranger}
-					<li>{ranger}</li>
-				{/each}
-			</ol>
-		</div>
-
-		<div class="group">
-			<h1 class="title is-size-4 mb-3">Mages</h1>
-			<ol>
-				{#each mages as mage}
-					<li>{mage}</li>
-				{/each}
-			</ol>
-		</div>
-
-		<div class="group">
-			<h1 class="title is-size-4 mb-3">Shamans</h1>
-			<ol>
-				{#each shamans as shaman}
-					<li>{shaman}</li>
-				{/each}
-			</ol>
-		</div>
+		<h1>Can't show, Currenly on war</h1>
+		<p>kekw</p>
 	</div>
-	</div>
+</div>
 
 <style>
 	.members {
