83903e7d6a482cb96a95b7476ca8e6b753c0bfd6
qxb3 <qxbthree@gmail.com>
2023-07-09 23:13:26 +0800

    added rules to vars.js

--- src/lib/vars.js
+++ src/lib/vars.js
@@ -4,9 +4,57 @@ export const links = [
   { href: '/rules', text: 'Rules', icon: 'fas fa-scroll' },
 ]
 
+export const rules = [
+  {
+    title: 'English Language Only',
+    full: 'This is a Nordic lead guild, and we communicate exclusively in English in guild-chats.'
+  },
+  {
+    title: 'Respect and Consequences',
+    full: 'We expect respectful behavior from all players, and any inappropriate actions will have consequences. Zero tolerance against racism and sexism.'
+  },
+  {
+    title: 'Membership Voting Process',
+    full: 'A person can become a guild member after successfully passing the voting process, which requires 18 hours or a majority vote.'
+  },
+  {
+    title: 'Guild Information on Discord',
+    full: 'Every guild member must regularly read the information published on Discord when they are able to do so.'
+  },
+  {
+    title: 'Supportive Community',
+    full: 'The guild aims to provide advice, assistance, and share experiences with those who need it.'
+  },
+  {
+    title: 'Responsible Expression of Opinions',
+    full: 'All players have the right to voice their opinions, as long as they are not irresponsible personal attacks made in the heat of the moment.'
+  },
+  {
+    title: 'Maintaining a Good Reputation',
+    full: 'Both inside and outside the guild, members should behave in a manner that does not tarnish the guild\'s reputation. This includes in-game channels like \'trade\' and \'local\' games with other players.'
+  },
+  {
+    title: 'Dispute Resolution',
+    full: 'Any problems arising from disputes between players should be reported to the guild leaders.'
+  },
+  {
+    title: 'KOS List and Player Hunt',
+    full: 'If a player is added to the KOS (kill on sight) list by the leaders, all members are required to add this player to their friendlist, report their locations, and actively hunt them until they have paid the consequences.'
+  },
+  {
+    title: 'Guild Colors',
+    full: 'Members should use the guild colors for their characters: black head and brown body. Alternate options include cyan head and brown body, brown head and black body, and dark red head and black body.'
+  },
+  {
+    title: 'Leaving and Rejoining',
+    full: 'Leaving Slackers without a valid reason or leaving to join another guild will result in a permanent ban from rejoining Slackers.'
+  }
+]
+
 export const discordLink = 'https://discord.gg/xKwBfkFkPw'
 
 export default {
   links,
+  rules,
   discordLink
 }
