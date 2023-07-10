3d1f358eab9c1ea02f331103f3000bbff5a1d82a
qxb3 <qxbthree@gmail.com>
2023-07-10 22:30:52 +0800

    change members link icon

--- src/lib/vars.js
+++ src/lib/vars.js
@@ -1,6 +1,6 @@
 export const links = [
   { href: '/', text: 'Home', icon: 'fas fa-home' },
-  { href: '/members', text: 'Members', icon: 'fas fa-user-group' },
+  { href: '/members', text: 'Members', icon: 'fas fa-users-crown' },
   { href: '/rules', text: 'Rules', icon: 'fas fa-scroll' },
 ]
 
